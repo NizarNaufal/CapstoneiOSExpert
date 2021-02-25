ba1d42483f9f10c33a38929d2f1c1b5df92d4336
NizarNaufal <32515100+NizarNaufal@users.noreply.github.com>
2021-02-25 17:42:51 +0700

    Initial Commit

---
+++ DicodingiOSExpert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DicodingiOSExpert
+//
+//  Created by nizar on 25/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
